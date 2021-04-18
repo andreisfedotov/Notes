749fd585064a1db51545d24e8577ca61061dcc60
Andrei Fedotov <andrei_fedotov@tutamail.com>
2021-04-18 03:55:13 +0300

    1. Clean architecture

---
+++ Notes.Domain/Note.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Notes.Domain
+{
+    public class Note
+    {
+        public Guid UserId { get; set; }
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime? EditDate { get; set; }
+    }
+}
