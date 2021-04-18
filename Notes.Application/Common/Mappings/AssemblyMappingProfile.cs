f509603a5de609c5f06c29e8d62f70f8188ecce9
Andrei Fedotov <andrei_fedotov@tutamail.com>
2021-04-18 04:24:27 +0300

    3. AutoMapper

---
+++ Notes.Application/Common/Mappings/AssemblyMappingProfile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using AutoMapper;
+
+namespace Notes.Application.Common.Mappings
+{
+    public class AssemblyMappingProfile : Profile
+    {
+        public AssemblyMappingProfile(Assembly assembly) =>
+            ApplyMappingsFromAssembly(assembly);
+
+        private void ApplyMappingsFromAssembly(Assembly assembly)
+        {
+            var types = assembly.GetExportedTypes()
+                .Where(type => type.GetInterfaces()
+                    .Any(i => i.IsGenericType &&
+                    i.GetGenericTypeDefinition() == typeof(IMapWith<>)))
+                .ToList();
+
+            foreach (var type in types)
+            {
+                var instance = Activator.CreateInstance(type);
+                var methodInfo = type.GetMethod("Mapping");
+                methodInfo?.Invoke(instance, new object[] { this });
+            }
+        }
+    }
+}
---
+++ Notes.Application/Common/Mappings/IMapWith.cs
@@ -0,0 +1,10 @@
+﻿using AutoMapper;
+
+namespace Notes.Application.Common.Mappings
+{
+    public interface IMapWith<T>
+    {
+        void Mapping(Profile profile) =>
+            profile.CreateMap(typeof(T), GetType());
+    }
+}
