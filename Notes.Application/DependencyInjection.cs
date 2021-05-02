91cb515709c746084e00817c7c97a7933fc08783
Andrei Fedotov <andrei_fedotov@tutamail.com>
2021-05-02 19:23:57 +0300

    5. Dependency Injection

---
+++ Notes.Application/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Notes.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(
+            this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
---
+++ Notes.Persistence/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Notes.Application.Interfaces;
+
+namespace Notes.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection
+            services, IConfiguration configuration)
+        {
+            var connectionString = configuration["DbConnection"];
+            services.AddDbContext<NotesDbContext>(options =>
+            {
+                options.UseSqlite(connectionString);
+            });
+            services.AddScoped<INotesDbContext>(provider =>
+                provider.GetService<NotesDbContext>());
+            return services;
+        }
+    }
+}
