fa8460043b41434ca94744c99a2829c4aa6b117d
Andrei Fedotov <andrei_fedotov@tutamail.com>
2021-05-27 00:44:33 +0300

    Update Identity Configuration

--- Notes.Identity/Notes.Identity/Configuration.cs
+++ Notes.Identity/Notes.Identity/Configuration.cs
@@ -35,22 +35,22 @@ namespace Notes.Identity
             {
                 new Client
                 {
-                    ClientId = "notes-web-api",
+                    ClientId = "notes-web-app",
                     ClientName = "Notes Web",
                     AllowedGrantTypes = GrantTypes.Code,
                     RequireClientSecret = false,
                     RequirePkce = true,
                     RedirectUris =
                     {
-                        "http://.../signin-oidc"
+                        "http://localhost:3000/signin-oidc"
                     },
                     AllowedCorsOrigins =
                     {
-                        "http://..."
+                        "http://localhost:3000"
                     },
                     PostLogoutRedirectUris =
                     {
-                        "http:/.../signout-oidc"
+                        "http://localhost:3000/signout-oidc"
                     },
                     AllowedScopes =
                     {
